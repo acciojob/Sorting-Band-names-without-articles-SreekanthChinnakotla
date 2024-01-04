fce897616554d931e1918846f1381b8d0ef6d7c3
SreekanthChinnakotla <134213780+SreekanthChinnakotla@users.noreply.github.com>
2024-01-04 19:24:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here
 
+let arr = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let mp ={};
+let reg = //bthe/b|/ban/b|/ba/b/gi;
+	let articlelessarray = [];
+for(let i in arr){
+	let articlelessitem = arr[i].replace(reg, "").trim();
+	articlelessarray.push(articlelessitem);
+	mp[articlelessitem]=arr[i]
+}
+articlelessarray.sort();
+let ans=[];
+for(let i of articlelessarray){
+	ans.push(mp[i])
+}
+console.log(ans);
